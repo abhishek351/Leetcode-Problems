b40e7f9a7848212a844f2d7cf314c393997d523d
Abhishek Yadav <you@example.com>
2021-12-11 16:05:10 +0530

    commit

---
+++ Merge Two Sorted List.py
@@ -0,0 +1,25 @@
+class Solution:
+    def mergeTwoLists(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        
+        dummy=ListNode(0)
+        temp=dummy
+        
+        if not l1:
+            return l2
+        if not l2:
+            return l1
+        
+        while l1 and l2:
+            if l1.val<l2.val:
+                temp.next=l1
+                l1=l1.next
+            else:
+                temp.next=l2
+                l2=l2.next
+            temp=temp.next
+           
+            if l1:
+                temp.next=l1
+            if l2:
+                temp.next=l2
+        return dummy.next
